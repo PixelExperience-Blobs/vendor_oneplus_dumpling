6b211399c58e0390e025c6e9b700802426260b08
itsjoeoui <maximeyxw@gmail.com>
2020-03-27 17:32:20 -0400

    msm8998-common: Sync up with op5-q/vendor_oneplus
    * https://github.com/op5-q/vendor_oneplus/commit/81be545a3299272824c60827e9b08aadd8a552d8
    * Credit: @poad42
    
    Signed-off-by: itsjoeoui <maximeyxw@gmail.com>

--- dumpling-vendor.mk
+++ dumpling-vendor.mk
@@ -19,6 +19,14 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/oneplus/dumpling
 
 PRODUCT_COPY_FILES += \
+    vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/EU/EU_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/EU/EU_Bluetooth_cal.acdb \
+    vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/EU/EU_General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/EU/EU_General_cal.acdb \
+    vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/EU/EU_Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/EU/EU_Global_cal.acdb \
+    vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/EU/EU_Handset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/EU/EU_Handset_cal.acdb \
+    vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/EU/EU_Hdmi_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/EU/EU_Hdmi_cal.acdb \
+    vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/EU/EU_Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/EU/EU_Headset_cal.acdb \
+    vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/EU/EU_Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/EU/EU_Speaker_cal.acdb \
+    vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/EU/EU_workspaceFile.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/EU/EU_workspaceFile.qwsp \
     vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/MTP/MTP_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP/MTP_Bluetooth_cal.acdb \
     vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/MTP/MTP_General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP/MTP_General_cal.acdb \
     vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/MTP/MTP_Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP/MTP_Global_cal.acdb \
@@ -29,6 +37,7 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/MTP/MTP_workspaceFile.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP/MTP_workspaceFile.qwsp \
     vendor/oneplus/dumpling/proprietary/vendor/etc/acdbdata/adsp_avs_config.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/adsp_avs_config.acdb \
     vendor/oneplus/dumpling/proprietary/vendor/etc/camera/imx398_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/imx398_chromatix.xml \
+    vendor/oneplus/dumpling/proprietary/vendor/etc/qdcm_calib_data_samsung_s6e3fc1_cmd_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_samsung_s6e3fc1_cmd_mode_dsi_panel.xml \
     vendor/oneplus/dumpling/proprietary/vendor/lib/hw/camera.msm8998.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/camera.msm8998.so \
     vendor/oneplus/dumpling/proprietary/vendor/lib/hw/fingerprint.fpc.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/fingerprint.fpc.so \
     vendor/oneplus/dumpling/proprietary/vendor/lib/hw/fingerprint.goodix.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/fingerprint.goodix.so \
@@ -37,10 +46,6 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/dumpling/proprietary/vendor/lib/libgf_ca.so:$(TARGET_COPY_OUT_VENDOR)/lib/libgf_ca.so \
     vendor/oneplus/dumpling/proprietary/vendor/lib/libgf_hal.so:$(TARGET_COPY_OUT_VENDOR)/lib/libgf_hal.so \
     vendor/oneplus/dumpling/proprietary/vendor/lib/libgoodixfingerprintd_binder.so:$(TARGET_COPY_OUT_VENDOR)/lib/libgoodixfingerprintd_binder.so \
-    vendor/oneplus/dumpling/proprietary/vendor/lib/libmmcamera2_imglib_modules.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera2_imglib_modules.so \
-    vendor/oneplus/dumpling/proprietary/vendor/lib/libmmcamera2_mct.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera2_mct.so \
-    vendor/oneplus/dumpling/proprietary/vendor/lib/libmmcamera2_sensor_modules.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera2_sensor_modules.so \
-    vendor/oneplus/dumpling/proprietary/vendor/lib/libmmcamera2_stats_modules.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera2_stats_modules.so \
     vendor/oneplus/dumpling/proprietary/vendor/lib/libmmcamera_imx398.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_imx398.so \
     vendor/oneplus/dumpling/proprietary/vendor/lib64/hw/fingerprint.fpc.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.fpc.so \
     vendor/oneplus/dumpling/proprietary/vendor/lib64/hw/fingerprint.goodix.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.goodix.so \
